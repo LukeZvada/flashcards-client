6a68121e64eaee1f8536b93e111e8551851a9d1f
LukeZvada <zvadamusic@gmail.com>
2021-02-03 13:17:34 -0600

    register is styled and now independent from login styling. Matches design.

--- src/components/auth/register.js
+++ src/components/auth/register.js
@@ -47,35 +47,35 @@ export const Register = (props) => {
                 <div>Passwords do not match</div>
                 <button className="button--close" onClick={e => passwordDialog.current.close()}>Close</button>
             </dialog>
-            <section className="login">
-                <form className="form--login" onSubmit={handleRegister}>
+            <section className="register">
+                <form className="form--register" onSubmit={handleRegister}>
                     <h2>Register an account</h2>
-                    <fieldset className="login--field">
+                    <fieldset className="register--field">
                         <label htmlFor="userName"> Username </label>
                         <input ref={userName} type="text" name="userName" className="form-control" placeholder="display name" />
                     </fieldset>
-                    <fieldset className="login--field">
+                    <fieldset className="register--field">
                         <label htmlFor="inputPassword"> Password </label>
                         <input ref={password} type="password" name="password" className="form-control" placeholder="password" required />
                     </fieldset>
-                    <fieldset className="login--field">
+                    <fieldset className="register--field">
                         <label htmlFor="verifyPassword"> Verify Password </label>
                         <input ref={verifyPassword} type="password" name="verifyPassword" className="form-control" placeholder="re-enter password" required />
                     </fieldset>
-                    <fieldset className="login--field">
+                    <fieldset className="register--field">
                         <label htmlFor="inputPassword"> First Name </label>
                         <input ref={firstName} type="text" name="first_name" className="form-control" placeholder="first name" required />
                     </fieldset>
-                    <fieldset className="login--field">
+                    <fieldset className="register--field">
                         <label htmlFor="inputPassword"> Last Name </label>
                         <input ref={lastName} type="text" name="last_name" className="form-control" placeholder="last name" required />
                     </fieldset>
-                    <fieldset className="login--field">
+                    <fieldset className="register--field">
                         <label htmlFor="inputPassword"> Email </label>
                         <input ref={email} type="text" name="email" className="form-control" placeholder="email" required />
                     </fieldset>
-                    <fieldset className="login--field">
-                        <button className="loginbutton" color="info" type="submit">Register</button>
+                    <fieldset className="register--field">
+                        <button className="register--button" color="info" type="submit">Register</button>
                     </fieldset>
                 </form>
                 <section className="link--register">
